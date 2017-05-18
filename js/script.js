2281b3304ed613993593fb6b6ba0b668c853ea28
Nevalainen Susanna <nevalas5@niksula.hut.fi>
2017-05-18 21:04:42 +0300

    more reports added to map, active button marked on bottom navbar

--- js/script.js
+++ js/script.js
@@ -26,8 +26,8 @@
     map = new google.maps.Map(document.getElementById('map'), {
       // center: {lat: 60.2045577, lng: 24.6568111}, Espoo
       center: {
-        lat: 60.171865,
-        lng: 24.923146
+        lat: 60.1976831,
+        lng: 24.9376494
       }, //Runeberginkatu 14-16
 
       zoom: 16,
@@ -264,8 +264,8 @@
       pos = {
         // lat: 60.2045577,
         // lng: 24.6568111
-        lat: 60.171865,
-        lng: 24.923146
+        lat: 60.1976831,
+        lng: 24.9376494
       };
     }
     var bikeLayer = new google.maps.BicyclingLayer();
@@ -284,6 +284,21 @@
     }, {
       position: new google.maps.LatLng(60.1770101, 24.7193514),
       type: 'attention'
+    }, {
+      position: new google.maps.LatLng(60.1976831,24.9376494),
+      type: 'attention'
+    }, {
+      position: new google.maps.LatLng(60.1978831,24.9396994),
+      type: 'suggestion'
+    }, {
+      position: new google.maps.LatLng(60.1976831,24.9376494),
+      type: 'attention'
+    }, {
+      position: new google.maps.LatLng(60.1956831,24.9516494),
+      type: 'suggestion'
+    }, {
+      position: new google.maps.LatLng(60.1970831,24.9396994),
+      type: 'fault'
     }, {
       position: new google.maps.LatLng(60.1718263, 24.9207661),
       type: 'attention'
@@ -450,12 +465,16 @@
     document.getElementById("pageTitle").innerHTML = "Send Feedback";
     document.getElementById("filter").style.display = "none";
     document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("map-navbar-btn").style.background = '#0151BC';
+    document.getElementById("report-navbar-btn").style.background= '#A2D7FB';
   }
 
   function showNews(){
     document.getElementById("news").style.display = 'block';
     document.getElementById("ranking").style.display = 'none';
     document.getElementById("filter").style.display = "block";
+    document.getElementById("map-navbar-btn").style.background = '#A2D7FB';
+    document.getElementById("report-navbar-btn").style.background = '#0151BC';
   }
 
   function showRanking(){
@@ -516,6 +535,81 @@
   }
   //================== GENERAL FUNCTIONS ==========================
 function prepareForKeyboard(){
-document.getElementById("navbar-bottom").style.position = absolute;
+//document.getElementById("navbar-bottom").style.position = absolute;
 
 }
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+     // feature. People can enter geographical searches. The search box will return a
+     // pick list containing a mix of places and predicted search terms.
+
+     // This example requires the Places library. Include the libraries=places
+     // parameter when you first load the API. For example:
+     // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+     function initAutocomplete() {
+       var map = new google.maps.Map(document.getElementById('map'), {
+         center: {lat: -33.8688, lng: 151.2195},
+         zoom: 13,
+         mapTypeId: 'roadmap'
+       });
+
+       // Create the search box and link it to the UI element.
+       var input = document.getElementById('pac-input');
+       var searchBox = new google.maps.places.SearchBox(input);
+       map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+       // Bias the SearchBox results towards current map's viewport.
+       map.addListener('bounds_changed', function() {
+         searchBox.setBounds(map.getBounds());
+       });
+
+       var markers = [];
+       // Listen for the event fired when the user selects a prediction and retrieve
+       // more details for that place.
+       searchBox.addListener('places_changed', function() {
+         var places = searchBox.getPlaces();
+
+         if (places.length == 0) {
+           return;
+         }
+
+         // Clear out the old markers.
+         markers.forEach(function(marker) {
+           marker.setMap(null);
+         });
+         markers = [];
+
+         // For each place, get the icon, name and location.
+         var bounds = new google.maps.LatLngBounds();
+         places.forEach(function(place) {
+           if (!place.geometry) {
+             console.log("Returned place contains no geometry");
+             return;
+           }
+           var icon = {
+             url: place.icon,
+             size: new google.maps.Size(71, 71),
+             origin: new google.maps.Point(0, 0),
+             anchor: new google.maps.Point(17, 34),
+             scaledSize: new google.maps.Size(25, 25)
+           };
+
+           // Create a marker for each place.
+           markers.push(new google.maps.Marker({
+             map: map,
+             icon: icon,
+             title: place.name,
+             position: place.geometry.location
+           }));
+
+           if (place.geometry.viewport) {
+             // Only geocodes have viewport.
+             bounds.union(place.geometry.viewport);
+           } else {
+             bounds.extend(place.geometry.location);
+           }
+         });
+         map.fitBounds(bounds);
+       });
+     }
